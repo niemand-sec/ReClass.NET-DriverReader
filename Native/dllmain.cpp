272f3d89ecaa0a3903159ccf620535cf60448080
KN4CK3R <admin@oldschoolhack.me>
2016-12-20 13:19:14 +0100

    Sync with ReClass.NET changes.

--- Native/dllmain.cpp
+++ Native/dllmain.cpp
@@ -1,41 +1,134 @@
 #include <windows.h>
 #include <cstdint>
 
-enum class RequestFunction
+#include <ReClassNET_Plugin.hpp>
+
+/// <summary>Enumerate all processes on the system.</summary>
+/// <param name="callbackProcess">The callback for a process.</param>
+void __stdcall EnumerateProcesses(EnumerateProcessCallback callbackProcess)
 {
-	IsProcessValid,
-	OpenRemoteProcess,
-	CloseRemoteProcess,
-	ReadRemoteMemory,
-	WriteRemoteMemory,
-	EnumerateProcesses,
-	EnumerateRemoteSectionsAndModules,
-	DisassembleRemoteCode,
-	ControlRemoteProcess
-};
-
-typedef LPVOID(__stdcall *RequestFunctionPtrCallback)(RequestFunction request);
-RequestFunctionPtrCallback requestFunction;
-
-/// <summary>This method gets called when ReClass.NET loads the plugin.</summary>
-VOID __stdcall Initialize(RequestFunctionPtrCallback requestCallback)
-{
-	requestFunction = requestCallback;
-
-	// Use requestFunction to get a function pointer from ReClass.NET to call the desired function.
-	//auto isProcessValid = static_cast<BOOL(__stdcall*)(HANDLE process)>(requestFunction(RequestFunction::IsProcessValid));
-	//if (isProcessValid(handle)) { ... }
+	// Enumerate all processes with the current plattform (x86/x64) and call the callback.
 }
 
-/// <summary>This function doesn't read memory but fills the buffer with 0-4.</summary>
-BOOL __stdcall ReadRemoteMemory(HANDLE process, LPCVOID address, LPVOID buffer, SIZE_T size)
+/// <summary>Enumerate all sections and modules of the remote process.</summary>
+/// <param name="process">The process handle obtained by OpenRemoteProcess.</param>
+/// <param name="callbackSection">The callback for a section.</param>
+/// <param name="callbackModule">The callback for a module.</param>
+void __stdcall EnumerateRemoteSectionsAndModules(RC_Pointer handle, EnumerateRemoteSectionsCallback callbackSection, EnumerateRemoteModulesCallback callbackModule)
 {
-	auto data = static_cast<uint8_t*>(buffer);
+	// Enumerate all sections and modules of the remote process and call the callback for them.
+}
 
-	for (auto i = 0u; i < size; ++i)
-	{
-		data[i] = i % 5;
-	}
+/// <summary>Opens the remote process.</summary>
+/// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
+/// <param name="desiredAccess">The desired access.</param>
+/// <returns>A handle to the remote process or nullptr if an error occured.</returns>
+RC_Pointer __stdcall OpenRemoteProcess(RC_Pointer id, ProcessAccess desiredAccess)
+{
+	// Open the remote process with the desired access rights and return the handle to use with the other functions.
 
-	return TRUE;
+	return nullptr;
+}
+
+/// <summary>Queries if the process is valid.</summary>
+/// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
+/// <returns>True if the process is valid, false if not.</returns>
+bool __stdcall IsProcessValid(RC_Pointer handle)
+{
+	// Check if the handle is valid.
+
+	return false;
+}
+
+/// <summary>Closes the handle to the remote process.</summary>
+/// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
+void __stdcall CloseRemoteProcess(RC_Pointer handle)
+{
+	// Close the handle to the remote process.
+}
+
+/// <summary>Reads memory of the remote process.</summary>
+/// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
+/// <param name="address">The address to read from.</param>
+/// <param name="buffer">The buffer to read into.</param>
+/// <param name="offset">The offset into the buffer.</param>
+/// <param name="size">The number of bytes to read.</param>
+/// <returns>True if it succeeds, false if it fails.</returns>
+bool __stdcall ReadRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
+{
+	// Read the memory of the remote process into the buffer.
+
+	return false;
+}
+
+/// <summary>Writes memory to the remote process.</summary>
+/// <param name="process">The process handle obtained by OpenRemoteProcess.</param>
+/// <param name="address">The address to write to.</param>
+/// <param name="buffer">The buffer to write.</param>
+/// <param name="offset">The offset into the buffer.</param>
+/// <param name="size">The number of bytes to write.</param>
+/// <returns>True if it succeeds, false if it fails.</returns>
+bool __stdcall WriteRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
+{
+	// Write the buffer into the memory of the remote process.
+
+	return false;
+}
+
+/// <summary>Control the remote process (Pause, Resume, Terminate).</summary>
+/// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
+/// <param name="action">The action to perform.</param>
+void __stdcall ControlRemoteProcess(RC_Pointer handle, ControlRemoteProcessAction action)
+{
+	// Perform the desired action on the remote process.
+}
+
+/// <summary>Attach a debugger to the process.</summary>
+/// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
+/// <returns>True if it succeeds, false if it fails.</returns>
+bool __stdcall AttachDebuggerToProcess(RC_Pointer id)
+{
+	// Attach a debugger to the remote process.
+
+	return false;
+}
+
+/// <summary>Detach a debugger from the remote process.</summary>
+/// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
+void __stdcall DetachDebuggerFromProcess(RC_Pointer id)
+{
+	// Detach the debugger.
+}
+
+/// <summary>Wait for a debug event within the given timeout.</summary>
+/// <param name="evt">[out] The occured debug event.</param>
+/// <param name="timeoutInMilliseconds">The timeout in milliseconds.</param>
+/// <returns>True if an event occured within the given timeout, false if not.</returns>
+bool __stdcall AwaitDebugEvent(DebugEvent* evt, int timeoutInMilliseconds)
+{
+	// Wait for a debug event.
+	
+	return false;
+}
+
+/// <summary>Handles the debug event described by evt.</summary>
+/// <param name="evt">[in] The (modified) event returned by AwaitDebugEvent.</param>
+void __stdcall HandleDebugEvent(DebugEvent* evt)
+{
+	// Handle the debug event.
+}
+
+/// <summary>Sets a hardware breakpoint.</summary>
+/// <param name="processId">The identifier of the process returned by EnumerateProcesses.</param>
+/// <param name="address">The address of the breakpoint.</param>
+/// <param name="reg">The register to use.</param>
+/// <param name="type">The type of the breakpoint.</param>
+/// <param name="size">The size of the breakpoint.</param>
+/// <param name="set">True to set the breakpoint, false to remove it.</param>
+/// <returns>True if it succeeds, false if it fails.</returns>
+bool __stdcall SetHardwareBreakpoint(RC_Pointer id, RC_Pointer address, HardwareBreakpointRegister reg, HardwareBreakpointTrigger type, HardwareBreakpointSize size, bool set)
+{
+	// Set a hardware breakpoint with the given parameters.
+
+	return false;
 }
