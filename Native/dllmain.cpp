5cb8a202bb397a0e8e8a6e671476eb1a4b510e84
KN4CK3R <admin@oldschoolhack.me>
2017-11-21 19:11:12 +0100

    Sync with ReClass.NET.

--- Native/dllmain.cpp
+++ Native/dllmain.cpp
@@ -1,15 +1,11 @@
-
-#ifndef __linux__
 #include <windows.h>
-#endif
-
 #include <cstdint>
 
 #include <ReClassNET_Plugin.hpp>
 
 /// <summary>Enumerate all processes on the system.</summary>
 /// <param name="callbackProcess">The callback for a process.</param>
-extern "C" void __stdcall EnumerateProcesses(EnumerateProcessCallback callbackProcess)
+extern "C" void RC_CallConv EnumerateProcesses(EnumerateProcessCallback callbackProcess)
 {
 	// Enumerate all processes with the current plattform (x86/x64) and call the callback.
 }
@@ -18,7 +14,7 @@ extern "C" void __stdcall EnumerateProcesses(EnumerateProcessCallback callbackPr
 /// <param name="process">The process handle obtained by OpenRemoteProcess.</param>
 /// <param name="callbackSection">The callback for a section.</param>
 /// <param name="callbackModule">The callback for a module.</param>
-extern "C" void __stdcall EnumerateRemoteSectionsAndModules(RC_Pointer handle, EnumerateRemoteSectionsCallback callbackSection, EnumerateRemoteModulesCallback callbackModule)
+extern "C" void RC_CallConv EnumerateRemoteSectionsAndModules(RC_Pointer handle, EnumerateRemoteSectionsCallback callbackSection, EnumerateRemoteModulesCallback callbackModule)
 {
 	// Enumerate all sections and modules of the remote process and call the callback for them.
 }
@@ -27,7 +23,7 @@ extern "C" void __stdcall EnumerateRemoteSectionsAndModules(RC_Pointer handle, E
 /// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
 /// <param name="desiredAccess">The desired access.</param>
 /// <returns>A handle to the remote process or nullptr if an error occured.</returns>
-extern "C" RC_Pointer __stdcall OpenRemoteProcess(RC_Pointer id, ProcessAccess desiredAccess)
+extern "C" RC_Pointer RC_CallConv OpenRemoteProcess(RC_Pointer id, ProcessAccess desiredAccess)
 {
 	// Open the remote process with the desired access rights and return the handle to use with the other functions.
 
@@ -37,7 +33,7 @@ extern "C" RC_Pointer __stdcall OpenRemoteProcess(RC_Pointer id, ProcessAccess d
 /// <summary>Queries if the process is valid.</summary>
 /// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
 /// <returns>True if the process is valid, false if not.</returns>
-extern "C" bool __stdcall IsProcessValid(RC_Pointer handle)
+extern "C" bool RC_CallConv IsProcessValid(RC_Pointer handle)
 {
 	// Check if the handle is valid.
 
@@ -46,7 +42,7 @@ extern "C" bool __stdcall IsProcessValid(RC_Pointer handle)
 
 /// <summary>Closes the handle to the remote process.</summary>
 /// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
-extern "C" void __stdcall CloseRemoteProcess(RC_Pointer handle)
+extern "C" void RC_CallConv CloseRemoteProcess(RC_Pointer handle)
 {
 	// Close the handle to the remote process.
 }
@@ -58,7 +54,7 @@ extern "C" void __stdcall CloseRemoteProcess(RC_Pointer handle)
 /// <param name="offset">The offset into the buffer.</param>
 /// <param name="size">The number of bytes to read.</param>
 /// <returns>True if it succeeds, false if it fails.</returns>
-extern "C" bool __stdcall ReadRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
+extern "C" bool RC_CallConv ReadRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
 {
 	// Read the memory of the remote process into the buffer.
 
@@ -72,7 +68,7 @@ extern "C" bool __stdcall ReadRemoteMemory(RC_Pointer handle, RC_Pointer address
 /// <param name="offset">The offset into the buffer.</param>
 /// <param name="size">The number of bytes to write.</param>
 /// <returns>True if it succeeds, false if it fails.</returns>
-extern "C" bool __stdcall WriteRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
+extern "C" bool RC_CallConv WriteRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
 {
 	// Write the buffer into the memory of the remote process.
 
@@ -82,7 +78,7 @@ extern "C" bool __stdcall WriteRemoteMemory(RC_Pointer handle, RC_Pointer addres
 /// <summary>Control the remote process (Pause, Resume, Terminate).</summary>
 /// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
 /// <param name="action">The action to perform.</param>
-extern "C" void __stdcall ControlRemoteProcess(RC_Pointer handle, ControlRemoteProcessAction action)
+extern "C" void RC_CallConv ControlRemoteProcess(RC_Pointer handle, ControlRemoteProcessAction action)
 {
 	// Perform the desired action on the remote process.
 }
@@ -90,7 +86,7 @@ extern "C" void __stdcall ControlRemoteProcess(RC_Pointer handle, ControlRemoteP
 /// <summary>Attach a debugger to the process.</summary>
 /// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
 /// <returns>True if it succeeds, false if it fails.</returns>
-extern "C" bool __stdcall AttachDebuggerToProcess(RC_Pointer id)
+extern "C" bool RC_CallConv AttachDebuggerToProcess(RC_Pointer id)
 {
 	// Attach a debugger to the remote process.
 
@@ -99,7 +95,7 @@ extern "C" bool __stdcall AttachDebuggerToProcess(RC_Pointer id)
 
 /// <summary>Detach a debugger from the remote process.</summary>
 /// <param name="id">The identifier of the process returned by EnumerateProcesses.</param>
-extern "C" void __stdcall DetachDebuggerFromProcess(RC_Pointer id)
+extern "C" void RC_CallConv DetachDebuggerFromProcess(RC_Pointer id)
 {
 	// Detach the debugger.
 }
@@ -108,7 +104,7 @@ extern "C" void __stdcall DetachDebuggerFromProcess(RC_Pointer id)
 /// <param name="evt">[out] The occured debug event.</param>
 /// <param name="timeoutInMilliseconds">The timeout in milliseconds.</param>
 /// <returns>True if an event occured within the given timeout, false if not.</returns>
-extern "C" bool __stdcall AwaitDebugEvent(DebugEvent* evt, int timeoutInMilliseconds)
+extern "C" bool RC_CallConv AwaitDebugEvent(DebugEvent* evt, int timeoutInMilliseconds)
 {
 	// Wait for a debug event.
 
@@ -117,7 +113,7 @@ extern "C" bool __stdcall AwaitDebugEvent(DebugEvent* evt, int timeoutInMillisec
 
 /// <summary>Handles the debug event described by evt.</summary>
 /// <param name="evt">[in] The (modified) event returned by AwaitDebugEvent.</param>
-extern "C" void __stdcall HandleDebugEvent(DebugEvent* evt)
+extern "C" void RC_CallConv HandleDebugEvent(DebugEvent* evt)
 {
 	// Handle the debug event.
 }
@@ -130,7 +126,7 @@ extern "C" void __stdcall HandleDebugEvent(DebugEvent* evt)
 /// <param name="size">The size of the breakpoint.</param>
 /// <param name="set">True to set the breakpoint, false to remove it.</param>
 /// <returns>True if it succeeds, false if it fails.</returns>
-extern "C" bool __stdcall SetHardwareBreakpoint(RC_Pointer id, RC_Pointer address, HardwareBreakpointRegister reg, HardwareBreakpointTrigger type, HardwareBreakpointSize size, bool set)
+extern "C" bool RC_CallConv SetHardwareBreakpoint(RC_Pointer id, RC_Pointer address, HardwareBreakpointRegister reg, HardwareBreakpointTrigger type, HardwareBreakpointSize size, bool set)
 {
 	// Set a hardware breakpoint with the given parameters.
 
