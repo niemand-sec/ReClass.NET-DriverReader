30e1f3119a31636d0880f6912fa0e4298bf5ec07
Niemand <noguera.sistemas@gmail.com>
2019-10-13 15:11:12 -0300

    Working version with Driver (still need a handle yet for some actions)

---
+++ Native/DriverReader.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <winternl.h>
+
+#pragma comment( lib, "ntdll.lib" )
+
+#define STATUS_INFO_LENGTH_MISMATCH      ((NTSTATUS)0xC0000004L)
+
+// GIO Driver
+#define IOCTL_GIO_MAPPHYSICAL	0xC3502004
+#define IOCTL_GIO_UNMAPPHYSICAL 0xC3502008
+#define IOCTL_GIO_MEMCPY 0xC3502808
+
+
+// Kernel offsets
+#define OFFSET_DIRECTORYTABLEBASE 0x028
+#define OFFSET_UNIQUEPROCESSID 0x2e8
+#define OFFSET_ACTIVEPROCESSLINKS 0x2f0
+#define OFFSET_VIRTUALSIZE 0x338
+#define OFFSET_SECTIONBASEADDRESS 0x3c0
+#define OFFSET_OBJECTTABLE 0x418
+#define OFFSET_IMAGEFILENAME 0x450
+#define OFFSET_PRIORITYCLASS 0x45f
+
+// Structure of MAP
+typedef struct _READ_REQUEST {
+	DWORD InterfaceType;
+	DWORD Bus;
+	ULONG64 PhysicalAddress;
+	DWORD IOSpace;
+	DWORD size;
+} READ_REQUEST;
+
+typedef struct _WRITE_REQUEST {
+	DWORDLONG address;
+	DWORD length;
+	DWORDLONG buffer;
+} WRITE_REQUEST;
+
+typedef struct _MEMCPY_REQUEST {
+	ULONG64 dest;
+	ULONG64 src;
+	DWORD size;
+} MEMCPY_REQUEST;
+
+struct SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX			// Size => 28
+{
+	PVOID Object;									// Size => 4 Offset =>0
+	ULONG UniqueProcessId;							// Size => 4 Offset =>4
+	ULONG HandleValue;								// Size => 4 Offset =>8
+	ULONG GrantedAccess;							// Size => 4 Offset =>12
+	USHORT CreatorBackTraceIndex;					// Size => 2 Offset =>16
+	USHORT ObjectTypeIndex;							// Size => 2 Offset =>18
+	ULONG HandleAttributes;							// Size => 4 Offset =>20
+	ULONG Reserved;									// Size => 4 Offset =>24
+};
+
+struct SYSTEM_HANDLE_INFORMATION_EX					// Size => 36
+{
+	ULONG NumberOfHandles;							// Size => 4 Offset => 0
+	ULONG Reserved;									// Size => 4 Offset => 4
+	SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX Handles[1];	// Size => 36 Offset => 8
+};
+
+
+class DriverReader
+{
+public:
+	static int getDeviceHandle(LPTSTR name);
+	static bool ReadPhyMemory(uintptr_t physicalAddress, LPVOID  lpBuffer, SIZE_T  nSize, SIZE_T  *lpNumberOfBytesRead);
+	static bool ReadVirtualMemory(uint64_t directoryTableBase, uintptr_t virtualAddress, LPCVOID lpBuffer, SIZE_T  nSize, SIZE_T  *lpNumberOfBytesRead);
+	static uintptr_t ObtainKProcessPointer(uint64_t directoryTableBase, std::vector<uintptr_t> pKernelPointers);
+	static uintptr_t GetKProcess(uintptr_t &directoryTableBase);
+	static uintptr_t SearchKProcess(LPCVOID processName, uintptr_t &directoryTableBase, uintptr_t pKProcess);
+	static bool ObtainKProcessInfo(uintptr_t &directoryTableBase, uintptr_t pKProcessAddress);
+	static bool LeakKernelPointers(std::vector<uintptr_t> &pKernelPointers);
+	static uintptr_t FindDirectoryBase();
+
+	// Variables
+	static HANDLE hDeviceDrv;
+	static char targetProc[256];
+	static char previousTargetProc[256];
+	static uintptr_t DTBTargetProcess;
+	static uintptr_t virtualSizeTargetProcess;
+	static uintptr_t pBaseAddressTargetProcess;
+};
+
--- Native/dllmain.cpp
+++ Native/dllmain.cpp
@@ -1,21 +1,79 @@
 #include <cstdint>
 
-#include <ReClassNET_Plugin.hpp>
+#if (_MSC_VER >= 1915)
+#define no_init_all deprecated
+#endif
 
-/// <summary>Enumerate all processes on the system.</summary>
-/// <param name="callbackProcess">The callback for a process.</param>
-extern "C" void RC_CallConv EnumerateProcesses(EnumerateProcessCallback callbackProcess)
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <vector>
+#include <algorithm>
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <psapi.h>
+#include <ReClassNET_Plugin.hpp>
+#include <experimental/filesystem>
+#include "DriverReader.h"
+namespace fs = std::experimental::filesystem;
+
+#if (_MSC_VER >= 1915)
+#define no_init_all deprecated
+#endif
+
+
+enum class Platform
 {
-	// Enumerate all processes with the current plattform (x86/x64) and call the callback.
+	Unknown,
+	X86,
+	X64
+};
+
+Platform GetProcessPlatform(HANDLE process)
+{
+	static USHORT processorArchitecture = PROCESSOR_ARCHITECTURE_UNKNOWN;
+	if (processorArchitecture == PROCESSOR_ARCHITECTURE_UNKNOWN)
+	{
+		SYSTEM_INFO info = {};
+		GetNativeSystemInfo(&info);
+
+		processorArchitecture = info.wProcessorArchitecture;
+	}
+
+	switch (processorArchitecture)
+	{
+	case PROCESSOR_ARCHITECTURE_INTEL:
+		return Platform::X86;
+	case PROCESSOR_ARCHITECTURE_AMD64:
+		auto isWow64 = FALSE;
+		if (IsWow64Process(process, &isWow64))
+		{
+			return isWow64 ? Platform::X86 : Platform::X64;
+		}
+
+#ifdef RECLASSNET64
+		return Platform::X64;
+#else
+		return Platform::X86;
+#endif
+	}
+	return Platform::Unknown;
 }
 
-/// <summary>Enumerate all sections and modules of the remote process.</summary>
-/// <param name="process">The process handle obtained by OpenRemoteProcess.</param>
-/// <param name="callbackSection">The callback for a section.</param>
-/// <param name="callbackModule">The callback for a module.</param>
-extern "C" void RC_CallConv EnumerateRemoteSectionsAndModules(RC_Pointer handle, EnumerateRemoteSectionsCallback callbackSection, EnumerateRemoteModulesCallback callbackModule)
+std::string getFileName(const std::string& s)
 {
-	// Enumerate all sections and modules of the remote process and call the callback for them.
+	char sep = '/';
+
+#ifdef _WIN32
+	sep = '\\';
+#endif
+
+	size_t i = s.rfind(sep, s.length());
+	if (i != std::string::npos) {
+		return(s.substr(i + 1, s.length() - i));
+	}
+	return("");
 }
 
 /// <summary>Opens the remote process.</summary>
@@ -25,8 +83,28 @@ extern "C" void RC_CallConv EnumerateRemoteSectionsAndModules(RC_Pointer handle,
 extern "C" RC_Pointer RC_CallConv OpenRemoteProcess(RC_Pointer id, ProcessAccess desiredAccess)
 {
 	// Open the remote process with the desired access rights and return the handle to use with the other functions.
+	DWORD access = STANDARD_RIGHTS_REQUIRED | PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION | SYNCHRONIZE;
+	switch (desiredAccess)
+	{
+	case ProcessAccess::Read:
+		access |= PROCESS_VM_READ;
+		break;
+	case ProcessAccess::Write:
+		access |= PROCESS_VM_OPERATION | PROCESS_VM_WRITE;
+		break;
+	case ProcessAccess::Full:
+		access |= PROCESS_VM_READ | PROCESS_VM_OPERATION | PROCESS_VM_WRITE;
+		break;
+	}
 
-	return nullptr;
+	const auto handle = OpenProcess(access, FALSE, static_cast<DWORD>(reinterpret_cast<size_t>(id)));
+
+	if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
+	{
+		return nullptr;
+	}
+
+	return handle;
 }
 
 /// <summary>Queries if the process is valid.</summary>
@@ -35,8 +113,18 @@ extern "C" RC_Pointer RC_CallConv OpenRemoteProcess(RC_Pointer id, ProcessAccess
 extern "C" bool RC_CallConv IsProcessValid(RC_Pointer handle)
 {
 	// Check if the handle is valid.
+	if (handle == nullptr)
+	{
+		return false;
+	}
 
-	return false;
+	const auto retn = WaitForSingleObject(handle, 0);
+	if (retn == WAIT_FAILED)
+	{
+		return false;
+	}
+
+	return retn == WAIT_TIMEOUT;
 }
 
 /// <summary>Closes the handle to the remote process.</summary>
@@ -44,7 +132,225 @@ extern "C" bool RC_CallConv IsProcessValid(RC_Pointer handle)
 extern "C" void RC_CallConv CloseRemoteProcess(RC_Pointer handle)
 {
 	// Close the handle to the remote process.
+	if (handle == nullptr)
+	{
+		return;
+	}
+
+	CloseHandle(handle);
 }
+
+
+/// <summary>Enumerate all processes on the system.</summary>
+/// <param name="callbackProcess">The callback for a process.</param>
+extern "C" void RC_CallConv EnumerateProcesses(EnumerateProcessCallback callbackProcess)
+{
+	// With this trick we'll be able to print content to the console, and if we have luck we could get information printed by the game.
+	AllocConsole();
+	SetConsoleTitle("Debug");
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+	freopen("CONIN$", "r", stdin);
+	
+	
+	// Enumerate all processes with the current plattform (x86/x64) and call the callback.
+	if (callbackProcess == nullptr)
+	{
+		return;
+	}
+
+	const auto handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32W pe32 = {};
+		pe32.dwSize = sizeof(PROCESSENTRY32W);
+		if (Process32FirstW(handle, &pe32))
+		{
+			do
+			{
+				const auto process = OpenRemoteProcess(reinterpret_cast<RC_Pointer>(static_cast<size_t>(pe32.th32ProcessID)), ProcessAccess::Read);
+				if (IsProcessValid(process))
+				{
+					const auto platform = GetProcessPlatform(process);
+#ifdef RECLASSNET64
+					if (platform == Platform::X64)
+#else
+					if (platform == Platform::X86)
+#endif
+					{
+						EnumerateProcessData data = { };
+						data.Id = pe32.th32ProcessID;
+						GetModuleFileNameExW(process, nullptr, reinterpret_cast<LPWSTR>(data.Path), PATH_MAXIMUM_LENGTH);
+						const auto name = fs::path(data.Path).filename().u16string();
+						str16cpy(data.Name, name.c_str(), std::min<size_t>(name.length(), PATH_MAXIMUM_LENGTH - 1));
+
+						callbackProcess(&data);
+					}
+
+				}
+
+				CloseRemoteProcess(process);
+
+			} while (Process32NextW(handle, &pe32));
+		}
+
+		CloseHandle(handle);
+	}
+
+}
+
+
+uintptr_t directoryTableBase = 0;
+uintptr_t pKProcess = 0;
+uintptr_t pBaseAddress = 0;
+
+/// <summary>Enumerate all sections and modules of the remote process.</summary>
+/// <param name="process">The process handle obtained by OpenRemoteProcess.</param>
+/// <param name="callbackSection">The callback for a section.</param>
+/// <param name="callbackModule">The callback for a module.</param>
+void RC_CallConv EnumerateRemoteSectionsAndModules(RC_Pointer handle, EnumerateRemoteSectionsCallback callbackSection, EnumerateRemoteModulesCallback callbackModule)
+{
+	// std::cout << "[.] EnumerateRemoteSectionsAndModules " << handle << std::endl;
+	// Enumerate all sections and modules of the remote process and call the callback for them.
+	
+	if (callbackSection == nullptr && callbackModule == nullptr && !DriverReader::DTBTargetProcess)
+	{
+		std::cout << "[-] EnumerateRemoteSectionsAndModules failed" << std::endl;
+		return;
+	}
+
+	std::vector<EnumerateRemoteSectionData> sections;
+
+	MEMORY_BASIC_INFORMATION memInfo = { };
+	memInfo.RegionSize = 0x1000;
+	size_t address = 0;
+	while (VirtualQueryEx(handle, reinterpret_cast<LPCVOID>(address), &memInfo, sizeof(MEMORY_BASIC_INFORMATION)) != 0 && address + memInfo.RegionSize > address)
+	{
+		if (memInfo.State == MEM_COMMIT)
+		{
+			EnumerateRemoteSectionData section = {};
+			section.BaseAddress = memInfo.BaseAddress;
+			section.Size = memInfo.RegionSize;
+
+			//std::cout << "[-] section.BaseAddress failed" << section.BaseAddress << std::endl;
+
+			section.Protection = SectionProtection::NoAccess;
+			if ((memInfo.Protect & PAGE_EXECUTE) == PAGE_EXECUTE) section.Protection |= SectionProtection::Execute;
+			if ((memInfo.Protect & PAGE_EXECUTE_READ) == PAGE_EXECUTE_READ) section.Protection |= SectionProtection::Execute | SectionProtection::Read;
+			if ((memInfo.Protect & PAGE_EXECUTE_READWRITE) == PAGE_EXECUTE_READWRITE) section.Protection |= SectionProtection::Execute | SectionProtection::Read | SectionProtection::Write;
+			if ((memInfo.Protect & PAGE_EXECUTE_WRITECOPY) == PAGE_EXECUTE_WRITECOPY) section.Protection |= SectionProtection::Execute | SectionProtection::Read | SectionProtection::CopyOnWrite;
+			if ((memInfo.Protect & PAGE_READONLY) == PAGE_READONLY) section.Protection |= SectionProtection::Read;
+			if ((memInfo.Protect & PAGE_READWRITE) == PAGE_READWRITE) section.Protection |= SectionProtection::Read | SectionProtection::Write;
+			if ((memInfo.Protect & PAGE_WRITECOPY) == PAGE_WRITECOPY) section.Protection |= SectionProtection::Read | SectionProtection::CopyOnWrite;
+			if ((memInfo.Protect & PAGE_GUARD) == PAGE_GUARD) section.Protection |= SectionProtection::Guard;
+
+			switch (memInfo.Type)
+			{
+			case MEM_IMAGE:
+				section.Type = SectionType::Image;
+				break;
+			case MEM_MAPPED:
+				section.Type = SectionType::Mapped;
+				break;
+			case MEM_PRIVATE:
+				section.Type = SectionType::Private;
+				break;
+			}
+
+			section.Category = section.Type == SectionType::Private ? SectionCategory::HEAP : SectionCategory::Unknown;
+
+			sections.push_back(std::move(section));
+		}
+		address = reinterpret_cast<size_t>(memInfo.BaseAddress) + memInfo.RegionSize;
+	}
+
+	const auto handle2 = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetProcessId(handle));
+	if (handle2 != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32W me32 = {};
+		me32.dwSize = sizeof(MODULEENTRY32W);
+		if (Module32FirstW(handle2, &me32))
+		{
+			do
+			{
+				if (callbackModule != nullptr)
+				{
+					EnumerateRemoteModuleData data = {};
+					data.BaseAddress = me32.modBaseAddr;
+					data.Size = me32.modBaseSize;
+					std::memcpy(data.Path, me32.szExePath, PATH_MAXIMUM_LENGTH * sizeof(RC_UnicodeChar));
+
+					callbackModule(&data);
+				}
+
+				if (callbackSection != nullptr)
+				{
+					auto it = std::lower_bound(std::begin(sections), std::end(sections), static_cast<LPVOID>(me32.modBaseAddr), [&sections](const auto& lhs, const LPVOID& rhs)
+					{
+						return lhs.BaseAddress < rhs;
+					});
+
+					IMAGE_DOS_HEADER DosHdr = {};
+					IMAGE_NT_HEADERS NtHdr = {};
+
+					//ReadRemoteMemory(handle, me32.modBaseAddr, &DosHdr, 0, sizeof(IMAGE_DOS_HEADER));
+					//ReadRemoteMemory(handle, me32.modBaseAddr + DosHdr.e_lfanew, &NtHdr, 0, sizeof(IMAGE_NT_HEADERS));
+
+					DriverReader::ReadVirtualMemory(directoryTableBase, reinterpret_cast<uintptr_t>(me32.modBaseAddr), &DosHdr, sizeof(IMAGE_DOS_HEADER), NULL);
+					DriverReader::ReadVirtualMemory(directoryTableBase, reinterpret_cast<uintptr_t>(me32.modBaseAddr + DosHdr.e_lfanew), &NtHdr, sizeof(IMAGE_NT_HEADERS), NULL);
+
+					std::vector<IMAGE_SECTION_HEADER> sectionHeaders(NtHdr.FileHeader.NumberOfSections);
+					//ReadRemoteMemory(handle, me32.modBaseAddr + DosHdr.e_lfanew + sizeof(IMAGE_NT_HEADERS), sectionHeaders.data(), 0, NtHdr.FileHeader.NumberOfSections * sizeof(IMAGE_SECTION_HEADER));
+					DriverReader::ReadVirtualMemory(directoryTableBase, reinterpret_cast<uintptr_t>(me32.modBaseAddr + DosHdr.e_lfanew + sizeof(IMAGE_NT_HEADERS)), sectionHeaders.data(), NtHdr.FileHeader.NumberOfSections * sizeof(IMAGE_SECTION_HEADER), NULL);
+
+					for (auto i = 0; i < NtHdr.FileHeader.NumberOfSections; ++i)
+					{
+						auto&& sectionHeader = sectionHeaders[i];
+
+						const auto sectionAddress = reinterpret_cast<size_t>(me32.modBaseAddr) + sectionHeader.VirtualAddress;
+						for (auto j = it; j != std::end(sections); ++j)
+						{
+							if (sectionAddress >= reinterpret_cast<size_t>(j->BaseAddress) && sectionAddress < reinterpret_cast<size_t>(j->BaseAddress) + static_cast<size_t>(j->Size))
+							{
+								// Copy the name because it is not null padded.
+								char buffer[IMAGE_SIZEOF_SHORT_NAME + 1] = { 0 };
+								std::memcpy(buffer, sectionHeader.Name, IMAGE_SIZEOF_SHORT_NAME);
+
+								if (std::strcmp(buffer, ".text") == 0 || std::strcmp(buffer, "code") == 0)
+								{
+									j->Category = SectionCategory::CODE;
+								}
+								else if (std::strcmp(buffer, ".data") == 0 || std::strcmp(buffer, "data") == 0 || std::strcmp(buffer, ".rdata") == 0 || std::strcmp(buffer, ".idata") == 0)
+								{
+									j->Category = SectionCategory::DATA;
+								}
+
+								MultiByteToUnicode(buffer, j->Name, IMAGE_SIZEOF_SHORT_NAME);
+								std::memcpy(j->ModulePath, me32.szExePath, PATH_MAXIMUM_LENGTH * sizeof(RC_UnicodeChar));
+
+								break;
+							}
+						}
+
+					}
+				}
+			} while (Module32NextW(handle2, &me32));
+		}
+
+		CloseHandle(handle2);
+
+		if (callbackSection != nullptr)
+		{
+			for (auto&& section : sections)
+			{
+				callbackSection(&section);
+			}
+		}
+	}
+
+	
+}
+
 
 /// <summary>Reads memory of the remote process.</summary>
 /// <param name="handle">The process handle obtained by OpenRemoteProcess.</param>
@@ -56,6 +362,69 @@ extern "C" void RC_CallConv CloseRemoteProcess(RC_Pointer handle)
 extern "C" bool RC_CallConv ReadRemoteMemory(RC_Pointer handle, RC_Pointer address, RC_Pointer buffer, int offset, int size)
 {
 	// Read the memory of the remote process into the buffer.
+	
+	if (handle)
+	{
+		//TCHAR Buffer[MAX_PATH];
+		//std::cout << "\t[.] handle " << handle << std::endl;
+		//std::cout << "\t[.] targetProc " << DriverReader::targetProc << std::endl;
+
+		if (GetProcessImageFileNameA(handle, DriverReader::targetProc, sizeof(DriverReader::targetProc)))
+		{
+			//std::cout << "\t[.] targetProc " << DriverReader::targetProc << std::endl;
+			strcpy(DriverReader::targetProc,getFileName(DriverReader::targetProc).c_str());
+			//std::cout << "\t[.] targetProc " << DriverReader::targetProc << std::endl;
+		}
+		else
+		{
+			//std::cout << "\t[.] targetProc failed: 0x" << std::hex << GetLastError() << std::endl;
+		}
+		//CloseHandle(handle);
+	}
+
+
+	if (strcmp(DriverReader::targetProc, DriverReader::previousTargetProc) != 0)
+	{
+		if (DriverReader::getDeviceHandle("\\\\.\\GIO"))
+		{
+			std::cout << "[-] Driver not loaded" << std::endl;
+			return false;
+		}
+		strcpy(DriverReader::previousTargetProc, DriverReader::targetProc);
+		
+		pKProcess = DriverReader::GetKProcess(directoryTableBase);
+
+		pBaseAddress = DriverReader::SearchKProcess(DriverReader::targetProc, directoryTableBase, pKProcess);
+
+		if (!DriverReader::ObtainKProcessInfo(directoryTableBase, pBaseAddress))
+		{
+			std::cout << "[-] ObtainKProcessInfo failed" << std::endl;
+		}
+
+	}
+
+	//std::cout << "[+] Reading ########################################" << std::endl;
+	//std::cout << "[+] directoryTableBase" << directoryTableBase << std::endl;
+	//std::cout << "[+] pKProcess" << pKProcess << std::endl;
+	//std::cout << "[+] pBaseAddress" << pBaseAddress << std::endl;
+	//std::cout << "[+] address" << address << std::endl;
+	//std::cout << "[+] size" << size << std::endl;
+	
+	buffer = reinterpret_cast<RC_Pointer>(reinterpret_cast<uintptr_t>(buffer) + offset);
+	//std::cout << "[+] buffer" << buffer << std::endl;
+	//std::cout << "[+] buffer" << DriverReader::DTBTargetProcess << std::endl;
+
+	SIZE_T numberOfBytesRead;
+	if (DriverReader::ReadVirtualMemory(DriverReader::DTBTargetProcess, reinterpret_cast<uintptr_t>(address), buffer, size, &numberOfBytesRead))
+	{
+		return true;
+	}
+
+	/*
+	if (ReadProcessMemory(handle, address, buffer, size, &numberOfBytesRead) && size == numberOfBytesRead)
+	{
+		return true;
+	}*/
 
 	return false;
 }
